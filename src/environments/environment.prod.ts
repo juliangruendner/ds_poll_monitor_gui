aa792f32fc652b21cbd81fbc5e7abab8f166ad61
Julian Gründner <juliangruendner@googlemail.com>
2018-09-11 12:01:06 +0200

    change prod build for test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import * as env from './.env.json';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://api.chucknorris.io',
+  serverUrl: 'http://141.67.22.10:5000',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
